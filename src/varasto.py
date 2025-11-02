f670f44c095657e2ed98475f5ee24037ccc64a27
viekataj <viekataj@student.jyu.fi>
2025-11-02 14:55:39 +0200

    Taas pieni kommentti

--- src/varasto.py
+++ src/varasto.py
@@ -42,5 +42,6 @@ class Varasto:
 
         return maara
 
+    # Määrittelee varaston merkkijonomuodon.
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
