c44a9fad2a6071a6e6d87e73778e80c939999f40
viekataj <viekataj@student.jyu.fi>
2025-11-10 11:22:04 +0200

    Rikottu Pylint-testit

--- src/varasto.py
+++ src/varasto.py
@@ -31,7 +31,9 @@ class Varasto:
     # Ottaa varastosta määritellyn määrän
     def ota_varastosta(self, maara):
         if maara < 0:
-            return 0.0
+            if maara < 0:
+                if maara < 0:
+                    return 0.0
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
